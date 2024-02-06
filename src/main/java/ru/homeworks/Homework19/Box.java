d3bbc9bfa62cbdad633aa73354307ad5bd3d8b5e
Alsunya <89420639+Alsunya@users.noreply.github.com>
2024-02-06 21:13:28 +0300

    Homework19. Generics. (#16)
    * Homework19. Generics.
    
    * Homework20. Java IO. Часть 2. Потоки ввода/вывода. Fixes.
    
    * Homework20. Java IO. Часть 2. Потоки ввода/вывода. Fixes.

---
+++ src/main/java/ru/homeworks/Homework19/Box.java
@@ -0,0 +1,28 @@
+package src.main.java.ru.homeworks.Homework19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> fruits;
+    public Box(List<T> fruits) {
+        this.fruits = new ArrayList<>(fruits);
+    }
+    public void addFruit(T fruit){
+        this.fruits.add(fruit);
+    }
+    public Integer weight(){
+        int weight = 0;
+        for (T fruit: this.fruits) {
+            weight += fruit.weight;
+        }
+        return weight;
+    }
+    public boolean compare(Box box){
+        return this.weight().equals(box.weight());
+    }
+    public void putInto(Box<T> box){
+        box.fruits.addAll(this.fruits);
+        this.fruits.clear();
+    }
+}
